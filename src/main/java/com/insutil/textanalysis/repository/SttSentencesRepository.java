79af6e8a259154c944739d52e60e77958e161c56
Daniel <csbon00@gmail.com>
2021-03-09 08:10:06 +0900

    morpheme tagging with stt contents

--- src/main/java/com/insutil/textanalysis/repository/SttSentencesRepository.java
+++ src/main/java/com/insutil/textanalysis/repository/SttSentencesRepository.java
@@ -1,10 +1,15 @@
 package com.insutil.textanalysis.repository;
 
 import com.insutil.textanalysis.model.SttSentences;
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface SttSentencesRepository extends R2dbcRepository<SttSentences, Long> {
-
+    @Modifying
+    @Query("delete from t_ta_stt_sentences where call_date = :callDate")
+    Mono<Integer> deleteSttSentencesByCallDate(String callDate);
 }
