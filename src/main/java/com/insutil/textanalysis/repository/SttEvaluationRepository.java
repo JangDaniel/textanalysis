d700f7b6524edb45c3b4d3d868f57b8b7c9715a8
ghyoo <ghyoo@insutil.com>
2021-04-01 08:55:07 +0900

    추가 : 날짜와 상담사 id 를 기준으로 stt evaluation 조회 추가

--- src/main/java/com/insutil/textanalysis/repository/SttEvaluationRepository.java
+++ src/main/java/com/insutil/textanalysis/repository/SttEvaluationRepository.java
@@ -6,11 +6,23 @@ import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDate;
+
 public interface SttEvaluationRepository extends R2dbcRepository<SttEvaluation, Long> {
-	@Query("select e.* from t_ta_stt_evaluation e" +
-		" left join t_ta_stt_contents s on s.id = e.stt_id" +
-		" where s.call_date = :callDate")
+	@Query(
+		"select e.* from t_ta_stt_evaluation e " +
+		"left join t_ta_stt_contents s on s.id = e.stt_id " +
+		"where s.call_date = :callDate"
+	)
 	Flux<SttEvaluation> findAllByCallDate(String callDate);
+
+	@Query(
+		"select e.* from t_ta_stt_evaluation e " +
+		" left join t_ta_stt_contents s on s.id = e.stt_id " +
+		" where s.call_date = :callDate " +
+		" and s.agent_id = :agentId"
+	)
+	Flux<SttEvaluation> findAllByCallDateAndAgentId(LocalDate callDate, Long agentId);
 
 	Mono<SttEvaluation> findBySttId(Long sttId);
 }
